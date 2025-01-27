da576c549086c92534166f6bf29d1c2cb9fa2290
Aman Chhabra <amanchhabra@192.168.1.104>
2025-01-27 19:44:09 +0000

    Update setup.py package name and add Gemini model support

--- setup.py
+++ setup.py
@@ -32,12 +32,12 @@ with open('README.md') as f:
 
 
 setuptools.setup(
-    name='gdm-concordia',
+    name='concordia',
     version='1.8.10',
     license='Apache 2.0',
     license_files=['LICENSE'],
-    url='https://github.com/google-deepmind/concordia',
-    download_url='https://github.com/google-deepmind/concordia/releases',
+    url='https://github.com/AMC-NEO/concordia',
+    download_url='https://github.com/AMC-NEO/concordia/releases',
     author='DeepMind',
     author_email='noreply@google.com',
     description=(
